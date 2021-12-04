7e1364618000c92ee6322e027764c928b72e20c1
damouryouknow <bailey@damour.xyz>
2021-12-04 10:38:24 -0500

    Add day 4

---
+++ 04/part-1.js
@@ -0,0 +1,57 @@
+const { access } = require('fs');
+const utils = require('../utils');
+
+utils.readData('./04/input').then((data) => {
+    const numbers = data[0].split(',').map((number) => parseInt(number));
+    const boards = chunks(data.slice(1), 5).map((board) => {
+        return board.map((row) => {
+            return row.split(' ').filter((token) => token != '').map((val) => {
+                return {value: parseInt(val), marked: false};
+            });
+        });
+    });
+    const score = tally(boards, numbers);
+    console.log(score);
+}).catch(console.error);
+
+function tally(boards, numbers) {
+    for (const number of numbers) {
+        for (const board of boards) {
+            if (markBoard(board, number)) {
+                return number * board
+                    .reduce((a, c) => [...a, ...c])
+                    .filter((tile) => !tile.marked)
+                    .map((tile) => tile.value)
+                    .reduce((a, c) => a + c);
+            }
+        }
+    }
+}
+
+function markBoard(board, number) {
+    const tile = board
+        .reduce((a, c) => [...a, ...c])
+        .find((tile) => tile.value == number);
+    if (tile) tile.marked = true;
+    return checkBoard(board);
+}
+
+function checkBoard(board) {
+    const transpose = board[0].map((x, i) => board.map(x => x[i]));
+    const winningRow = board.find((row) => {
+        return row.filter((tile) => !tile.marked).length == 0;
+    });
+    if (winningRow) return true;
+    const winningCol = transpose.find((col) => {
+        return col.filter((tile) => !tile.marked).length == 0;
+    });
+    return winningCol;
+}
+
+function chunks(arr, size) {
+    const result = [];
+    for (let i = 0; i < arr.length; i = i + size) {
+        result.push(arr.slice(i, i + size));
+    }
+    return result;
+}
