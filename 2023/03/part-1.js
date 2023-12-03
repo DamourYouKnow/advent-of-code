e81d0a55415096f95336b6c362d31161195a8a52
Bailey D'Amour <bailey@damour.xyz>
2023-12-03 12:43:18 -0500

    Add 2023, day 3

---
+++ 2023/03/part-1.js
@@ -0,0 +1,81 @@
+const { off } = require('process');
+const utils = require('../utils');
+
+
+const neighborOffsets = [
+    { x: 1, y: 0 },
+    { x: 1, y: 1 },
+    { x: 0, y: 1 },
+    { x: -1, y: 1 },
+    { x: -1, y: 0 },
+    { x: -1, y: -1 },
+    { x: 0, y: -1 },
+    { x: 1, y: -1 }
+];
+
+
+utils.readData('./03/input').then((data) => {
+    const grid = data.map((row) => row.split(''));
+
+    const width = grid[0].length;
+    const height = grid.length;
+
+    const hasPartNeighbor = (x, y) => {
+        return neighborOffsets.some((offest) => {
+            const checkX = x + offest.x;
+            const checkY = y + offest.y;
+
+            if (checkX < 0 || checkX >= width) {
+                return false;
+            }
+            if (checkY < 0 || checkY >= height) {
+                return false;
+            }
+
+            const value = grid[checkY][checkX];
+            return value != '.' && isNaN(value);
+        })
+    };
+
+    const readNumber = (x, y) => {
+        const positions = [];
+        for (let xOffset = x; x < width; xOffset++) {
+            const value = Number(grid[y][xOffset]);
+            if (!isNaN(value)) {
+                positions.push({ x: xOffset, y: y });
+            }
+            else {
+                break;
+            }
+        }
+
+        return positions;
+    };
+
+    const numberPositions = [];
+
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            const positions = readNumber(x, y);
+            if (positions.length > 0) {
+                const isPart = positions.some((position) => {
+                    return hasPartNeighbor(position.x, position.y);
+                });
+
+                if (isPart) {
+                    numberPositions.push(positions);
+                }
+
+                x += positions.length - 1;
+            }
+        }
+    }
+
+    const numbers = numberPositions.map((positions) => {
+        return Number(positions.reduce((a, c) => a + grid[c.y][c.x], ''));
+    });
+
+    const sum = numbers.reduce((a, c) => a + c, 0);
+
+    console.log(sum);
+}).catch(console.error);
---
+++ 2023/03/part-2.js
@@ -0,0 +1,85 @@
+const { off } = require('process');
+const utils = require('../utils');
+
+
+utils.readData('./03/input').then((data) => {
+    const grid = data.map((row) => row.split(''));
+
+    const width = grid[0].length;
+    const height = grid.length;
+
+    const readNumber = (x, y) => {
+        const positions = [];
+        for (let xOffset = x; x < width; xOffset++) {
+            const value = Number(grid[y][xOffset]);
+            if (!isNaN(value)) {
+                positions.push({ x: xOffset, y: y });
+            }
+            else {
+                break;
+            }
+        }
+
+        return positions;
+    };
+
+    const getNumberNeighbors = (x, y) => {
+        const numberNeighbors = [];
+
+        for (let yOffset = -1; yOffset <= 1; yOffset++) {
+            const checkY = y + yOffset;
+            if (checkY < 0 || checkY >= height) continue;
+
+            for (let xOffset = -1; xOffset <= 1; xOffset++) {
+                const checkX = x + xOffset;
+                if (checkX < 0 || checkX >= width) continue;
+                if (xOffset == 0 && yOffset == 0) continue;
+
+                const value = grid[checkY][checkX];
+                if (!isNaN(value)) {
+                    let backtrack = 0;
+                    while (!isNaN(grid[checkY][x + xOffset - backtrack - 1])) {
+                        backtrack++;
+                    }
+                    
+                    const positions = readNumber(
+                        x + xOffset - backtrack, 
+                        checkY
+                    );
+
+                    numberNeighbors.push(positions);
+                    xOffset += positions.length - backtrack;
+                }
+            }
+        }
+
+        return numberNeighbors;
+    };
+
+    const gears = [];
+
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            const value = grid[y][x];
+            if (value == '*') {
+                const numberNeighbors = getNumberNeighbors(x, y);
+                if (numberNeighbors.length == 2) {
+                    gears.push(numberNeighbors);
+                }
+            }
+        }
+    }
+
+    const ratios = gears.map((gear) => {
+        const numbers = gear.map((positions) => {
+            return Number(positions.reduce((a, c) => a + grid[c.y][c.x], ''));
+        });
+
+        return numbers.reduce((a, c) => a * c, 1);
+    });
+
+    const sum = ratios.reduce((a, c) => a + c, 0);
+
+    console.log(sum);
+
+}).catch(console.error);
