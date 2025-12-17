9867b2532b0a864ec2ded2aaabc3f6818b228c83
Bailey D'Amour <bailey@damour.xyz>
2025-12-17 10:16:17 -0500

    Add 2025, day 7

--- 2025/07/part-2.js
+++ 2025/07/part-2.js
@@ -1,3 +1,4 @@
+const { time } = require('console');
 const utils = require('../utils');
 
 utils.readData('./07/input', true).then((data) => {
@@ -5,13 +6,19 @@ utils.readData('./07/input', true).then((data) => {
     const startPosition = grid.findPosition((value) => value == 'S');
     grid.set(startPosition, '|');
 
-    let splits = 0;
+    // Memoize how many timelines impact position
+    const timelines = new utils.Grid(
+        new Array(grid.grid.length).fill(0),
+        grid.size
+    );
 
     for (let y = startPosition.y; y < grid.size.y; y++) {
         for (let x = 0; x < grid.size.x; x++) {
             const currentPosition = { x: x, y: y };
 
             if (grid.at(currentPosition) == '|') {
+                const visitedTimelines = timelines.at(currentPosition) || 1;
+
                 const below = { 
                     x: currentPosition.x, 
                     y: currentPosition.y + 1 
@@ -22,16 +29,21 @@ utils.readData('./07/input', true).then((data) => {
                     { x: below.x + 1, y: below.y }
                 ] : [below];
 
-                if (nextPositions.length == 2) splits +=1 ;
-
                 for (const position of nextPositions) {
-                    if (grid.at(position) == '.') {
-                        grid.set(position, '|');
-                    }
+                    grid.set(position, '|');
+
+                    timelines.set(
+                        position, 
+                        timelines.at(position) + visitedTimelines
+                    );
                 }
             }
         }
     }
 
-    console.log(splits);
+    const totalTimelines = timelines.row(timelines.size.y - 1)
+        .reduce((acc, value) => acc + value, 0);
+
+    console.log(timelines.output());
+    console.log(totalTimelines);
 }).catch(console.error);
